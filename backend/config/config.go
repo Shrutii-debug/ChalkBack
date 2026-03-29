602116d5441dd686c82228ff666ce3a8f306c004
Shrutii-debug <shrutiagarwal1467@gmail.com>
2026-03-30 00:30:35 +0530

    changed config

--- backend/config/config.go
+++ backend/config/config.go
@@ -15,7 +15,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Kolkata",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Kolkata",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
